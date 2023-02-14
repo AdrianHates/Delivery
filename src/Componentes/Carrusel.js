58e8a75f7ed3034af1f9548c99bab6a9e89bbd64
AizenSousuke <kurono1807@gmail.com>
2023-02-13 19:40:11 -0500

    fixed component Carrusel

--- src/Componentes/Carrusel.js
+++ src/Componentes/Carrusel.js
@@ -5,13 +5,11 @@ function Carrusel( { elemento } ) {
   return(
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        {elemento.map((x,i)=><button key={i} type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={`${i}`} aria-label={`Slide ${i+1}`} className={i === 0 ? "active" : ""} aria-current={i === 0 ? "true" : undefined}></button> )}
       </div>
       <div class="carousel-inner">
         <div class="carousel-inner">
-        {elemento.map((x,i)=><div class={i===0?"carousel-item active":"carousel-item"}>
+        {elemento.map((x,i)=><div key={i} class={i===0?"carousel-item active":"carousel-item"}>
           <img class="d-block w-100" src={x} alt={`${i} Slide`} />
         </div>)}      
         </div>
